45520a1b9d91aefe6e8c773cd4c77f94c192c154
hemalatha <hemalatha.chandaka@thundesoft.com>
2023-02-15 15:28:34 +0530

    good

---
+++ mathslibrary/src/main/java/com/example/mathslibrary/MathOps.java
@@ -0,0 +1,10 @@
+package com.example.mathslibrary;
+
+import android.content.Context;
+
+public class MathOps {
+
+    public static int add(Context context,int a,int b){
+        return  a+b;
+    }
+}
